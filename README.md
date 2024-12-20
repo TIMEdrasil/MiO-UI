dae85c826f88787851797b33a5fd7a8125696504
Mimiokish <Business.Jiahe.Lin@gmail.com>
2024-12-20 18:25:20 +0800

    update: examples->showcase

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ JavaScript + Rollup + Vue 3.0
 ```
 Dependencies installation: npm install
 
-Local debugging: npm run serve --projectName=[project name under examples]
+Local debugging: npm run serve --projectName=[project name under showcase]
 
 Packaging: npm run build
 ```
--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -5,7 +5,7 @@ import plugin_babel from "@rollup/plugin-babel";
 import plugin_scss from "rollup-plugin-scss";
 import plugin_serve from "rollup-plugin-serve";
 
-const PROJECT_BASE = process.env.npm_config_projectName ? "examples/" + process.env.npm_config_projectName : "static";
+const PROJECT_BASE = process.env.npm_config_projectName ? "showcase/" + process.env.npm_config_projectName : "static";
 
 export default {
     input: "src/index.js",
